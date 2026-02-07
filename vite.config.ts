b1fe005aeabd2b11a8fa4a73022d7e326867675f
Vinayak Kulkarni <19776877+vinayakkulkarni@users.noreply.github.com>
2026-02-08 04:14:59 +0530

    refactor: update vite.config.ts for ESM-only output

---
+++ vite.config.ts
@@ -0,0 +1,26 @@
+import vue from '@vitejs/plugin-vue';
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  plugins: [vue()],
+  build: {
+    target: 'esnext',
+    sourcemap: true,
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      formats: ['es'],
+      fileName: 'v-vectors',
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        exports: 'named',
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.name === 'style.css') return 'v-vectors.css';
+          return assetInfo.name;
+        },
+      },
+    },
+  },
+});
